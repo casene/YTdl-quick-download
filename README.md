a328a630ac1fc17661e56f661584b747ed0d2afc
adrz <adrz@mmrz.rz.lan>
2021-08-04 12:33:04 +0100

    add files to repo

---
+++ README.md
@@ -0,0 +1,31 @@
+## YTdl-quick-download
+
+#### Quick Description:
+
+A pretty simple bash script that downloads a Youtube video, the description file, the JSON info file and subs if avaialable. The resulting video and audio files are muxed to an mkv file. The file names are then sainitized and moved to a newly created directory with the same name as the downloaded video.
+
+#### Usage:
+
+Call the script with a URL as an argument.
+
+```bash
+./ytdl-qd "URL to your video"
+```
+
+That's pretty much it. You should now have a new directory in your `/tmp` directory with the downloaded files.
+
+
+
+#### Prerequisites:
+
+The script has the following prerequisites:
+
+- Youtube-dl
+
+- JQ
+  
+  The script checks for these prerequisites before executing anything. It will return an error message and exit with code 2 if the prerequisites are not found. 
+
+#### Testing:
+
+I have tried to make the script runnable on both OSX and various Linux flavours. Testing has been done on MacOS Catalina and Ubuntu Server 16.04.
---
+++ ytdl-qd.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env sh
+
+# Author: Casene
+# Date: 01 Aug 2021
+
+#### ---------------####
+
+
+
+# set OS environment
+
+OS=$(uname)
+
+# sanity check
+
+which jq > /dev/null 2>&1
+
+if [ ! $? -eq 0 ]
+    then
+        case $OS in
+            Darwin )
+                osascript -e 'display notification "Jq is not installed" with title "Error 404"'
+                exit 1
+                ;;
+            Linux )
+                echo "JQ is not installed, exiting with status 1"
+                exit 1
+                ;;
+            * )
+                echo "This script can be run on OSX or Linux only, exiting with status 1"
+                exit 1
+                ;;
+        esac
+fi
+
+which youtube-dl > /dev/null 2>&1
+
+if [ ! $? -eq 0 ]
+    then
+        case $OS in
+            Darwin )
+                osascript -e 'display notification "Youtube-dl is not installed" with title "Error 404"'
+                exit 1
+                ;;
+            Linux )
+                echo "Youtube-dl is not installed, exiting with status 1"
+                exit 1
+                ;;
+            * )
+                echo "This script is designed for use in OSX or Linux only, exiting with status 1"
+                exit 1
+                ;;
+        esac
+fi
+
+## if you are here - checks returned okay
+## script starts here
+
+
+# script variables (Change this to your prefered location)
+downloadpath=/tmp
+
+
+# ytdl command arguments (add,remove or modify to your liking)
+# the whole command is redirected to stdout to a file named meta in /tmp
+if [ $OS = "Darwin" ]
+    then
+        logpath=("~/Library/Logs/youtube-dl-downloads.log")
+elif [ $OS = "Linux" ]
+    then
+        logpath=("~/youtube-dl-downloads.log")
+fi
+
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' \
+-o "$downloadpath/%(title)s" \
+--restrict-filenames \
+--write-sub \
+--merge-output-format 'mkv' \
+--write-info-json \
+--write-description \
+--print-json \
+--download-archive \
+${logpath[@]} \
+"$1" > "$downloadpath/meta"
+
+
+# check ytdl exit status. if 0 then execute if block else exit
+# with error 2
+if [ $? -eq 0 ]
+    then
+        title=$(basename $(jq -r '._filename' $downloadpath/meta))
+
+        newtitle=$(echo $title | Sed -E \
+        -e 's/_s_/s_/g' \
+        -e 's/_t_/t_/g' \
+        -e 's/^[^[:alnum:]]+//' \
+        -e 's/[^[:alnum:]]$//' \
+        -e 's/[^[:alnum:]]+/-/g')
+
+        newtitle=$(echo $newtitle | tr '[:upper:]' '[:lower:]')
+    else
+        case $OS in
+            Darwin )
+                osascript -e 'display notification "Ytdl exited with errors, check /tmp dir for downloaded files" with title "Fatal Error"'
+                exit 1
+                ;;
+            Linux )
+                echo "Ytdl exited with errors, check /tmp dir for downloaded files"
+                exit 1
+                ;;
+        esac
+fi
+
+
+# create yt file directory
+mkdir $downloadpath/$newtitle
+
+
+
+# loop through files in /tmp and move them to dir if
+#they match the regex $title
+for file in $(ls $downloadpath/)
+do
+if [[ $file =~ $title ]]
+then
+    mv "$downloadpath/$file" "$downloadpath/$newtitle/$newtitle.${file##*.}"
+fi
+done
+
+
+# cleanup
+rm $downloadpath/meta
+
+# There should now be a directory with the name of your video
+# in the /tmp directory with the video, description, subs(if available)
+# and json info file.
+
+
